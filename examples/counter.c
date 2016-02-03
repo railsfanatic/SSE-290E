7a4eb62a8f035cb24a964b65a2ae370c904d1aea
Tom Grushka <tom@grushka.com>
2016-02-03 15:17:15 -0700

    More examples from Feb 3

---
+++ examples/counter.c
@@ -0,0 +1,13 @@
+/*	counter.c:
+	For loop demo.
+	by Tom Grushka
+	February 3, 2016	*/
+
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	for (int i=1; i<=5; i++) {
+		printf("The counter is %d.\n", i);
+	}
+}
---
+++ examples/mark.c
@@ -0,0 +1,46 @@
+/*	mark.c:
+	Display mark range for entered letter grade.
+	by Tom Grushka
+	February 3, 2016	*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, const char *argv[])
+{
+	char grade;
+	
+	if (argc == 2) {
+		strncpy(&grade, argv[1], 1);
+	} else {
+		printf("Enter a letter grade: ");
+		fgets(&grade, 1, stdin);
+	}
+	
+	printf("Grade: %c \n", grade);
+	
+	switch (grade) {
+		case 'A':
+		case 'a':
+			printf("The mark is 90 to 100.\n");
+			break;
+		case 'B':
+		case 'b':
+			printf("The mark is 80 to under 90.\n");
+			break;
+		case 'C':
+		case 'c':
+			printf("The mark is 70 to under 80.\n");
+			break;
+		case 'D':
+		case 'd':
+			printf("The mark is 60 to under 70.\n");
+			break;
+		case 'F':
+		case 'f':
+			printf("The mark is below 60.\n");
+			break;
+		default:
+			printf("Invalid grade entered.\n");
+	}
+}
