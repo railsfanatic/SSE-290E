08f9ad3ab50efc70a9c72f717d2dc91d2490ca7d
Tom Grushka <tom@grushka.com>
2016-02-05 20:34:17 -0700

    multiples Unity test framework

--- homework2/multiples.c
+++ homework2/multiples.c
@@ -1,23 +1,11 @@
-#include "../Unity/src/unity.h"
-#include <stdio.h>
-#include <setjmp.h>
-
-extern void testWillAlwaysPass(void);
-extern void testWillAlwaysFail(void);
-
-void runTest(UnityTestFunction test)
-{
-	if (TEST_PROTECT())
-	{
-		test();
-	}
-}
+#include "multiples.h"
 
 int main()
 {
-	UnityBegin(__FILE__);
-	RUN_TEST(testWillAlwaysPass, __LINE__);
-	RUN_TEST(testWillAlwaysFail, __LINE__);
-	UnityEnd();
-	return 0;
+	printf("main\n");
+}
+
+int myInteger(void)
+{
+	return 5;
 }
---
+++ homework2/test/test_multiples.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "../multiples.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_myInteger(void)
+{
+  TEST_ASSERT_EQUAL(5, myInteger());
+}
