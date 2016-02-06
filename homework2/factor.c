9a1b8c8c7efb5bfc258b85122bd6fffa9954341a
Tom Grushka <tom@grushka.com>
2016-02-06 03:56:16 -0700

    Homework 2 almost done

--- homework2/factor.c
+++ homework2/factor.c
@@ -10,8 +10,6 @@
 #include <math.h>
 #include <time.h>
 
-long long getLongLong(const char *prompt);
-
 /*	Execution time from Thomas Pornin
 	http://stackoverflow.com/questions/5248915/execution-time-of-c-program	*/
 clock_t begin, end;
@@ -19,64 +17,77 @@ double time_spent;
 
 /*	Array type adapted from casablanca
 	http://stackoverflow.com/questions/3536153/c-dynamically-growing-array 	*/
-typedef struct {
-  long long *array;
-  size_t used;
-  size_t size;
-} Array;
-
-void initArray(Array *a, size_t initialSize) {
-  a->array = (long long *)malloc(initialSize * sizeof(long long));
-  a->used = 0;
-  a->size = initialSize;
-}
-
-void insertArray(Array *a, long long element) {
-  if (a->used == a->size) {
-    a->size *= 2;
-    a->array = (long long *)realloc(a->array, a->size * sizeof(long long));
-  }
-  a->array[a->used++] = element;
-}
-
-void freeArray(Array *a) {
-  free(a->array);
-  a->array = NULL;
-  a->used = a->size = 0;
-}
+	typedef struct {
+	  long long *array;
+	  size_t used;
+	  size_t size;
+	} Array;
+	
+	// function to initialise the array
+	void initArray(Array *a, size_t initialSize) {
+	  a->array = (long long *)malloc(initialSize * sizeof(long long));
+	  a->used = 0;
+	  a->size = initialSize;
+	}
+	
+	// function to add an item to the array
+	void insertArray(Array *a, long long element) {
+	  if (a->used == a->size) {
+	    a->size *= 2;
+	    a->array = (long long *)realloc(a->array, a->size * sizeof(long long));
+	  }
+	  a->array[a->used++] = element;
+	}
+	
+	// function to free the memory used by the array
+	void freeArray(Array *a) {
+	  free(a->array);
+	  a->array = NULL;
+	  a->used = a->size = 0;
+	}
 /*	End of Array type	*/
 
 Array prime;
 
+// 	declare functions
+long long getLongLong(const char *prompt);
 int checkPrime(long long number);
 
+/*	checkPrime: determine if the given number is prime;
+		if prime, add it to the prime Array and return 1;
+		if not, return 0.
+	arguments: number (long long)
+	returns: integer 0 (false) or 1 (true)	*/
 int checkPrime(long long number)
 {
-	int isPrime = 1;
-	int s = 0;
-	s = sqrt(number);
-	if (number == 1) return 0;
+	int isPrime = 1;	// flag (0 = false, 1 = true)
+	int sq = 0;			// store square root
+	sq = sqrt(number);	// get square root of number
+	if (number == 1) return 0;	// not a prime; prevent false negatives
+	//  these quick checks cut processing time a little
 	if (number % 2 == 0 || number % 3 == 0) return 0;
+	//  loop through the array:
 	for (int i = 0; i < prime.used; i++)
 	{
-		if (prime.array[i] > s) break;
+		if (prime.array[i] > sq) break;  // stopping at square root of the number saves a whole lot of time
 		if (number % prime.array[i] == 0) {
-			isPrime = 0;
-			break;
+			isPrime = 0; 	// divisible by another prime
+			break;			// don't check any more
 		}
 	}
+	// if prime, add to array
 	if (isPrime) insertArray(&prime, number);
-	return isPrime;
+	return isPrime;			// return the flag
 }
 
 int main(int argc, char** argv)
 {
-	long long number = 0;
-	long long sqroot = 0;
+	long long number = 0;	// input number (long long)
+	long long sqroot = 0;	// square root of input number (long long)
 	
-	int checkForPrime = 0;
-	int checkEvenNumbers = 0;
-	int printAllNumbers = 0;
+	int checkForPrime = 0;		// input - skip prime number checking if 0
+	int checkEvenNumbers = 0;	// input - skip even number checking if 0
+	int printAllNumbers = 0;	// input - print out all numbers, not just factors; or, print just primes
 	
 	int isPrime = 0;
 	initArray(&prime, 10);  // initially 10 spaces for prime numbers
@@ -93,7 +104,10 @@ int main(int argc, char** argv)
 	else
 		checkEvenNumbers = (int)getLongLong("Check Even Numbers? ");
 	
-	printAllNumbers = (int)getLongLong("Print All Numbers? ");
+	if (checkForPrime)
+		printAllNumbers = (int)getLongLong("Print All Prime Numbers? ");
+	else
+		printAllNumbers = (int)getLongLong("Print All Numbers? ");
 	
 	sqroot = sqrt(number);
 	
@@ -107,7 +121,7 @@ int main(int argc, char** argv)
 		if (checkForPrime) isPrime = checkPrime(factor);
 		if (number % factor == 0) {
 			printf("%30lld  %5s  %30lld\n", factor, (isPrime ? "Y" : ""), number / factor);
-		} else if (printAllNumbers) {
+		} else if ((checkForPrime && isPrime && printAllNumbers) || (!checkForPrime && printAllNumbers)) {
 			printf("%30lld  %5s\n", factor, (isPrime ? "Y" : ""));
 		}
 	}
