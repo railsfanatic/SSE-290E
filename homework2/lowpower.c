60adbc455a5ec2523c66475eebce707b0b784b97
Tom Grushka <tom@grushka.com>
2016-02-05 23:05:31 -0700

    Homework 2-1 and 2-2

---
+++ homework2/lowpower.c
@@ -0,0 +1,97 @@
+/*	HOMEWORK 2.2
+	lowpower.c:
+	Prompts the user for a VOLTAGE (float) & RESISTANCE (float).
+	Calculates POWER (float):
+		P = V^2 / R
+	If POWER EXCEEDS MAX_POWER (0.25) W, print error & start over.
+	Else print calculated POWER.
+	by Tom Grushka
+	February 5, 2016	*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+const float MAX_POWER = 0.25;	// Watts of MAX POWER allowed (0.25 in assignment spec)
+
+// A few quotes from 2001: Space Odyssey to throw at the user for entering bad values
+const char *QUOTES[6] = {
+	"I'm sorry, Dave. I'm afraid I can't do that.",
+	"I think you know what the problem is just as well as I do.",
+	"This mission is too important for me to allow you to jeopardize it.",
+	"I know that you and Frank were planning to fry me, and I'm afraid that's something I cannot allow to happen.",
+	"Daisy, Daisy, give me your answer do.",
+	"Dave, this conversation can serve no purpose anymore. Goodbye."
+};
+
+// Declare input function
+float getFloat(const char *prompt);
+
+int main(int argc, char** argv)
+{
+	float voltage = 0.0;	// store input voltage
+	float resistance = 0.0;	// store input resistance
+	float power = 0.0;		// store calculated power = voltage^2 / resistance
+	int quote = 0;			// quote "counter"
+	
+	// Print introduction to user
+	printf("Good afternoon, Dave. I am a HAL 9000 computer. I am afraid my power needs adjusting.\n");
+	
+	/*	Loop indefinitely if power is a "bad value":
+		less than zero or greater than MAX_POWER	*/
+	while (power <= 0 || power > MAX_POWER) {
+		// Print instructions
+		printf("Please specify a new voltage and resistance, Dave.\n");
+		printf("I cannot handle more than %0.2f Watts of power, Dave.\n", MAX_POWER);
+		
+		/*	Input loop: keep prompting user for voltage, then
+			 resistance, each until reasonable value entered. 	*/
+		do {
+			voltage = getFloat("Voltage: ");
+		} while (voltage <= 0);
+		do {
+			resistance = getFloat("Resistance: ");
+		} while (resistance <= 0);
+		
+		//	Calculate the power:	power = voltage ^2 / resistance
+		power = (pow(voltage, 2)) / resistance;
+		
+		//	Cannot exceed MAX_POWER!
+		if (power > MAX_POWER)
+		{
+			printf("\a\a\aPower limit exceeded! %s\n\n", QUOTES[quote]);
+			quote++;
+			// Give the user 6 tries, then give up (exit with an error)
+			if (quote == 6) exit(1);
+		}
+	}
+	
+	// Print positive feedback and result
+	printf("\nVery good, Dave. That will be %0.2f Watts of power. Have a nice day!\n", power);
+	
+	return 0;	// We shouldn't have an error
+}
+
+/*	getFloat
+	argument:	prompt (const char *)
+				string to prompt the user
+	return:		user input converted to float	*/
+float getFloat(const char *prompt)
+{
+	// Some code by David C. Rankin, http://stackoverflow.com/questions/27326839/how-to-read-input-of-unknown-length-using-fgets
+	char *input = NULL;		// input buffer
+	float myFloat = 0.0;	// floating point number to return
+    size_t n = 0;           // max chars to read (0 = no limit)
+    ssize_t nchr = 0;       // # chars read
+	
+	printf("%s", prompt);	// Display the prompt
+	
+	if ((nchr = getline(&input, &n, stdin)) != -1)
+		input[--nchr] = 0;	// strip newline
+	
+	myFloat = atof(input);	// convert to float & store
+	
+	free(input);			// "de-allocate" input variable
+	
+	return myFloat;			// return float
+}
---
+++ homework2/multiples.c
@@ -0,0 +1,67 @@
+/*	HOMEWORK 2.1
+	multiples.c:
+	Prompts the user for a POSITIVE INTEGER limit and prints all
+	multiples of 5 that DO NOT EXCEED the entered limit.
+	by Tom Grushka
+	February 5, 2016	*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+const int MULTIPLIER = 5;	// constant multiplier (5 given in assignment spec)
+
+int getInteger(const char *prompt);
+
+int main(int argc, char** argv)
+{
+	int limit = 0;		// user-specified positive integer to count up to
+	int i = 0;			// counter
+	int product = 0;	// product = counter * MULTIPLIER
+	
+	/*	Input loop: keep prompting user for limit until
+		a reasonable value is entered. 	*/
+	do {
+		limit = getInteger("Enter an integer greater than zero: ");
+	} while (limit <= 0);
+	
+	// Tell user what we're doing
+	printf("\nCounting to %d by multiples of %d:\n", limit, MULTIPLIER);
+	
+	/*	Begin output loop:
+		As long as i * MULTIPLIER <= limit ... */
+	while (product <= limit) {
+		printf("%5d * %1d = %7d\n", i, MULTIPLIER, product);  // pretty print
+		i++;	// increment counter
+		product = i * MULTIPLIER;	// calculate product
+	}	// if while condition not met, stop looping
+	
+	if (limit % MULTIPLIER > 0)	// check for remainder
+		// explain remainder to user
+		printf("I cannot reach %d because it is not a multiple of %d.\n", limit, MULTIPLIER);
+	
+	return 0;	// We shouldn't have an error
+}
+
+/*	getInteger
+	argument:	prompt (const char *)
+				string to prompt the user
+	return:		user input converted to integer	*/
+int getInteger(const char *prompt)
+{
+	// Some code by David C. Rankin, http://stackoverflow.com/questions/27326839/how-to-read-input-of-unknown-length-using-fgets
+	char *input = NULL;		// input buffer
+	int myInt = 0;			// integer to return
+    size_t n = 0;           // max chars to read (0 = no limit)
+    ssize_t nchr = 0;       // # chars read
+	
+	printf("%s", prompt);	// Display the prompt
+	
+	if ((nchr = getline(&input, &n, stdin)) != -1)
+		input[--nchr] = 0;	// strip newline
+	
+	myInt = atoi(input);	// convert to integer & store
+	
+	free(input);			// "de-allocate" input variable
+	
+	return myInt;			// return integer
+}
