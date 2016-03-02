7a21b89af5279fb4c017b3b0dd9175873778df27
Grushka <tgrushka@ms147o.winad.msudenver.edu>
2016-03-02 13:15:35 -0700

    working on hw 3

--- homework3/main.c
+++ homework3/main.c
@@ -1,54 +1,36 @@
-#include <sys/ioctl.h>
-#include <unistd.h>
-
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
-#include <stdlib.h>
 
-int fact(int n)
-{
-	if (n == 0) return 1;
-	return n*fact(n-1);
-}
-
-char pascal(int n)
-{
-	int r = 0;
-	int result = 0;
-	char *temp;
-	
-	for (r = 0; r <= n; r++)
-	{
-		result = fact(n)/(fact(r)*fact(n-r));
-		//temp += sprintf(temp, "%d  ", result);
-	}
-	//*temp += sprintf("\n");
-	return *temp;
-}
+#include "funcs.h"
 
 int main(int argc, char **argv)
 {
-	int i = 0;
-	int n = 0;
+	int choice = 0;
 	
-	int columns = strtol(getenv("COLUMNS"), NULL, 10);
-	
-	printf("Rows? ");
-	scanf("%d", &n);
-	
-	for (i = 0; i < n; i++)
-	{
-		for (int j = 0; j < (n - i); j++) printf(" ");
+	printf("\n\nHOMEWORK 3 MENU\n===============\n\n"
 		
-		pascal(i);
-	}
+		"1.  Smallest Random Number\n\n"
+		"2.  Random and Reverse\n\n"
+		"3.  Find Adjacent Match\n\n"
+		"4.  Guess the Password\n\n"
+		"5.  Count the Vowels\n\n"
+		"6.  Palindrome\n\n"
+		"7.  Hypotenuse\n\n"
+		"8.  Factorial\n\n"
+		"9.  Average with Upper Limit\n\n\n"
+		
+		"Please enter a number (1-9, other to quit). ");
 	
+	scanf(" %d", &choice);
+	printf("\n\n");
+	
+	switch(choice)
+	{
+		case 1:
+			printf("SMALLEST RANDOM NUMBER\n\n");
+			break;
+		default:
+			printf("Goodbye!\n\n");
+			break;
+	}
 	return 0;
 }
-
-/*
-	Pascal's Triangle
-	Binomial Expansion
-	
-*/
