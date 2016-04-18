471c06d8f342c9315d5732da30d2263b89ff016a
Tom Grushka <tom@grushka.com>
2016-04-18 15:08:50 -0600

    homework 5

--- homework5/grow.c
+++ homework5/grow.c
@@ -7,35 +7,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// initial size of the array
 #define INITIAL_SIZE 5
 
 int main()
 {
-	int *a = malloc(INITIAL_SIZE * sizeof(int));
-	int arraySize = INITIAL_SIZE;
-	int *a_start = a;
-	int *new;
-	int *new_start;
+	// declarations
+	int *a = malloc(INITIAL_SIZE * sizeof(int));	// allocate the array
+	int arraySize = INITIAL_SIZE;	// stores current array size (capacity)
+	int numElements = 0;	// stores the actual number of elements entered
+	int *a_start = a;	// save pointer at start
+	int *new;			// pointer to new array
+	int *new_start;		// pointer to start of new array
 	
+	// loop indefinitely until we enter 0
 	do {
-		scanf("%d", a);
-		a++;
+		scanf("%d", a);		// get integer from user
+		if (*a == 0) break;	// exit loop if we enter 0
+		numElements ++;		// increment # of array elements
+		a++;				// increment array pointer
+		// if we've filled the array...
 		if (a - a_start == arraySize)
 		{
+			// allocate a new array 2x the size of the existing one
 			new = malloc(2 * arraySize * sizeof(int));
+			// set start pointer equal to new pointer
 			new_start = new;
+			// copy the old array to the new one
 			for (a = a_start; a - a_start < arraySize; a++)
 			{
 				*new = *a;
 				new++;
 			}
-			free(a_start);
-			a_start = new_start;
-			a = a_start + arraySize;
-			arraySize *= 2;
+			free(a_start);	// free the old array
+			a_start = new_start;	// set a_start pointer to new array
+			a = a_start + arraySize;	// set "a" pointer to current position
+			arraySize *= 2;		// double array size value
+			// tell user we've doubled the size of the array
 			printf("Grew array to %d\n", arraySize);
 		}
-	} while (1);
+	} while (1);	// loop indefinitely
+	
+	// exit loop when user enters 0,
+	// then print the # of elements in the final array
+	printf("There are %d elements in the array.\n", numElements);
+	
+	// print the array in 5 columns
+	for (a = a_start; a - a_start < numElements; a++)
+	{
+		printf("%10d\t", *a);
+		if ((a - a_start) % 5 == 4) printf("\n");
+	}
+	printf("\n");
 	
 	return 0;
 }
--- homework5/wordcount.c
+++ homework5/wordcount.c
@@ -8,44 +8,49 @@
 #include <ctype.h>
 #include <string.h>
 
+// set default filename
 #define DEFAULT_FILENAME "test.txt"
 
 int main(int argc, char **argv)
 {
-	FILE *fp;
-	char c;
-	int wordCount = 0;
-	int inWord = 0;
-	char fileName[255];
+	// declarations
+	FILE *fp;				// file pointer
+	char c;					// stores char as we loop through file
+	int wordCount = 0;		// stores the word count
+	int inWord = 0;			// flags that we are currently "in" a word
+	char fileName[255];		// stores the filename
 	
-	if (argc == 1)
+	if (argc == 1)	// no filename passed as argument
 	{
 		printf("Please enter the file name:\n");
 		scanf("%s", fileName);
 	}
+	// filename passed as argument
 	else strcpy(fileName, argv[1]);
+	// blank filename -- set to default
 	if (strcmp(fileName, "") == 0) strcpy(fileName, DEFAULT_FILENAME);
 	
 	printf("Counting words in %s...\n", fileName);
-	fp = fopen(fileName, "r");
+	fp = fopen(fileName, "r");	// open the file
 	
-	if (fp != NULL)
+	if (fp != NULL)	// if fp not null, the file was opened successfully
 	{
-		c = fgetc(fp);
-		while (!feof(fp))
+		c = fgetc(fp);	// get first char in file
+		while (!feof(fp))	// loop until end of file
 		{
-			if (isspace(c)) inWord = 0;
-			else if (inWord == 0)
+			if (isspace(c)) inWord = 0;	// it's a space!
+			else if (inWord == 0)	// it's a word!
 			{
-				inWord = 1;
-				wordCount ++;
+				inWord = 1;	// set inWord flag until we get a space
+				wordCount ++;	// increment word count
 			}
-			c = getc(fp);
+			c = getc(fp);	// get next char in file
 		}
-		fclose(fp);
+		fclose(fp);		// close the file
+		// print the word count
 		printf("The word count is %d.\n", wordCount);
 	}
-	else
+	else	// the file could not be opened
 	{
 		printf("The file %s does not exist or cannot be opened.\n", fileName);
 	}
